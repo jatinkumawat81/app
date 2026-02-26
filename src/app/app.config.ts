1889d98e524dbc4d01c310ffe3a7287d56f58ec1
Jatin <jatinkumawat530@gmail.com>
2026-02-26 14:46:26 +0530

    adding interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './services/interceptor/jwt-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes,
+      withComponentInputBinding(),
+    ),
+    provideHttpClient(withFetch(), withInterceptors([jwtInterceptor])),
   ]
 };
